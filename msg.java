55aa62b9056e3161c2a7df3559202a222b86af4a
root <root@ip-172-31-28-150.ec2.internal>
2026-09-21 17:25:21 +0000

    gtaction

---
+++ msg.java
@@ -0,0 +1,11 @@
+public class Msg {
+    public static void main(String[] args){
+        int eno=1001;
+        String ename="KRISHNA";
+        String course="Python";
+        System.out.println("Employee Details are:");
+        System.out.println("Emp no is:"+eno);
+        System.out.println("Emp name is:" +ename);
+        System.out.println("My course is:" +course);
+    }
+}
